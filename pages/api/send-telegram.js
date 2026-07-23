76b6b12a2654d3ce7f2328a5d545ba272551eca2
Harsh-Nandal <harshnandal862005@gmail.com>
2026-07-23 22:15:54 +0530

    replacing bottoken and chat id

--- pages/api/send-telegram.js
+++ pages/api/send-telegram.js
@@ -8,6 +8,7 @@ import Students from '../../models/User'; // Assuming you have a Students model
 const BOT_TOKEN = '8072882753:AAGXU1N6E3ZDGHb91oxCWUaBZSRHaSvIzSY'; // Move to .env for security
 const CHAT_ID = '6693684914'; // Your Telegram ID or group/channel ID
 
+
 // const BOT_TOKEN = "8430452006:AAEgmLpUCqPCLLUaK-WxWvyz5iMXPOAgef0";
 // const CHAT_ID = 6251710308;
 
